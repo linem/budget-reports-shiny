a77cb19069c7acd15766c885c9a58c780c21132f
Line Møller <line.moller@gmail.com>
2024-04-29 13:59:23 +0200

    change name on table script

--- app.R
+++ app.R
@@ -6,13 +6,9 @@ library("bslib")
 library("shinyjs")
 library("plotly")
 
-
-# transactions_dt <- read_csv("data/mydata_2024-04-22.csv", show_col_types = FALSE) %>%
-#   select(date, category, subcategory, name, transaction, amount)
-
 source("ui.R")
 source("server.R")
 source("plots.R")
-source("tables2-0.R")
+source("tables.R")
 
 shinyApp(ui = ui, server = server)
